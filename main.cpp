8d2fcf33de448eb4e1d8fe5bf0e1a013c181b171
kn1cht <ito.turukasiwa@gmail.com>
2017-07-04 16:46:08 +0900

    StressCircle class implementation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <algorithm>
-#include <cassert>
+#include <climits>
 #include <cmath>
 #include <complex>
 #include <cstdio>
@@ -22,105 +22,57 @@
 #include <GL/glut.h>
 #include <GLUI/glui.h>
 #include "stress_circle.hpp"
-#define pb push_back
-#define mp make_pair
-#define fst first
-#define snd second
-#define all(v) v.begin(), v.end()
-#define debug(x) cerr<< #x <<": "<<x<<endl
-#define debug2(x,y) cerr<< #x <<": "<< x <<", "<< #y <<": "<< y <<endl
-#define debug3(x,y,z) cerr<< #x <<": "<< x <<", " << #y <<": "<< y <<", " << #z <<": "<< z << endl
+#define debug(x) std::cerr<< #x <<": "<<x<<std::endl
+#define debug2(x,y) std::cerr<< #x <<": "<< x <<", "<< #y <<": "<< y <<std::endl
 
-using namespace std;
+const unsigned int XNAME = 1;
+const unsigned int YNAME = 2;
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
 
-static const int Buf = 512;
-
-double handlePos[2][4] = {
-  {0.1,-0.1,-0.1, 0.1},
-  {0.1, 0.1,-0.1,-0.1}
-};
-
+bool is_drag = 0;
 int main_window;
-unsigned int width = 800;
-unsigned int height = 600;
-int mouse_button = -1;
-
-unsigned int object_number = 0;
-double object_depth = -1;
-bool move_flag = 0;
-
-GLUI_EditText *sigma_x_box, *sigma_y_box, *tau_box;
-struct StressCircle stressCircle;
+GLUI_Spinner *sigma_x_box, *sigma_y_box, *tau_box;
+GLUI_EditText *angle_box, *sigma_1_box, *sigma_2_box, *tau_max_box;
+struct StressCircle stressCircle(XNAME, YNAME, WIDTH, HEIGHT);
 
 void drawAxis(void);
-void drawHandle(GLenum mode);
-/**
- * Process mouse picking.
- * @param x mouse x coordination
- * @param y mouse y coordination
- */
-void pickMouse(int x, int y);
-/**
- * Render new image.
- */
 void display(void);
-/**
- * Mouse movement callback function.
- * @param x mouse x coordination
- * @param y mouse y coordination
- */
-void dragObject(int x, int y);
-/**
- * Window resize callback function.
- * @param w width of window
- * @param h height of window
- */
+void motion(int x, int y);
 void reshape (int w, int h);
-/**
- * Keyboard input callback function.
- * @param key key number
- * @param x mouse x coordination
- * @param y mouse y coordination
- */
 void keyboard (unsigned char key, int x, int y);
-/**
- * Mouse click callback function.
- * @param button button number
- * @param state button state
- * @param x mouse x coordination
- * @param y mouse y coordination
- */
 void mouse(int button, int state, int x, int y);
-/**
- * Idle callback function.
- */
 void idle();
-/**
- * Exit button callback function.
- */
-void gluiExit(int num);
+void gruiReset(int num);
 
 int main(int argc, char** argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode ( GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH );
-  glutInitWindowSize (width, height);
+  glutInitWindowSize (WIDTH, HEIGHT);
   glutInitWindowPosition (50, 50);
   main_window = glutCreateWindow("Main");
   GLUI_Master.set_glutDisplayFunc(display);
   GLUI_Master.set_glutKeyboardFunc(keyboard);
   GLUI_Master.set_glutMouseFunc(mouse);
-  glutMotionFunc(dragObject);
+  glutMotionFunc(motion);
   GLUI_Master.set_glutReshapeFunc(reshape);
   GLUI_Master.set_glutIdleFunc(idle);
   GLUI glui = *(GLUI_Master.create_glui_subwindow(main_window, GLUI_SUBWINDOW_BOTTOM));
   glui.set_main_gfx_window(main_window);
-  sigma_x_box = glui.add_edittext("sigma_x", GLUI_EDITTEXT_FLOAT, &stressCircle.sigma_x, 1);
+  sigma_x_box = glui.add_spinner("sigma_x", GLUI_SPINNER_FLOAT, &stressCircle.sigma_x);
+  sigma_y_box = glui.add_spinner("sigma_y", GLUI_SPINNER_FLOAT, &stressCircle.sigma_y);
+  tau_box = glui.add_spinner("tau", GLUI_SPINNER_FLOAT, &stressCircle.tau);
   glui.add_column(true);
-  sigma_y_box = glui.add_edittext("sigma_y", GLUI_EDITTEXT_FLOAT, &stressCircle.sigma_y, 2);
+  angle_box = glui.add_edittext("angle", GLUI_SPINNER_FLOAT, &stressCircle.angle);
+  sigma_1_box = glui.add_edittext("sigma_1", GLUI_EDITTEXT_FLOAT, &stressCircle.sigma_1);
   glui.add_column(true);
-  tau_box = glui.add_edittext("tau", GLUI_EDITTEXT_FLOAT, &stressCircle.tau, 3);
+  sigma_2_box = glui.add_edittext("sigma_2", GLUI_EDITTEXT_FLOAT, &stressCircle.sigma_2);
+  tau_max_box = glui.add_edittext("tau_max", GLUI_EDITTEXT_FLOAT, &stressCircle.tau_max);
   glui.add_column(true);
-  glui.add_button("Exit", 0, gluiExit);
+  glui.add_translation("Scale", GLUI_TRANSLATION_X, NULL, 1);
+  glui.add_column(true);
+  glui.add_button("Reset", 0, gruiReset);
+  glui.add_button("Exit", 0, exit);
   glClearColor (0.3, 0.3, 0.3, 0.0); // set clear color
   glEnable(GL_DEPTH_TEST);
   glEnable(GL_CULL_FACE);
@@ -130,74 +82,21 @@ int main(int argc, char** argv) {
   return EXIT_SUCCESS;
 }
 
-void drawHandle(GLenum mode) {
-  if(mode == GL_SELECT) { glLoadName(1); }
-  glColor3f(1.0, 0.0, 0.0);
-  glBegin(GL_QUADS);
-  for(int i = 0; i < 4; ++i) {
-    glVertex2d(stressCircle.sigma_x + handlePos[0][i], stressCircle.tau + handlePos[1][i]);
-  }
-  glEnd();
-  if(mode == GL_SELECT) { glLoadName(2); }
-   glPointSize(3);
-  glColor3f(0.0, 0.0, 1.0);
-  glBegin(GL_QUADS);
-  for(int i = 0; i < 4; ++i) {
-    glVertex2d(stressCircle.sigma_y + handlePos[0][i], -stressCircle.tau + handlePos[1][i]);
-  }
-  glEnd();
-  glFlush();
-}
-
 void drawAxis(void) {
-  double axis[][3] = {
-    {-5.0, 0.0, 0.0 },
-    { 5.0, 0.0, 0.0 },
-    { 0.0,-5.0, 0.0 },
-    { 0.0, 5.0, 0.0 }
+  double axis[2][2][3] = {
+    {{-5.0, 0.0,-0.02}, { 5.0, 0.0,-0.02}},
+    {{ 0.0,-5.0,-0.02}, { 0.0, 5.0,-0.02}}
   };
-  for(int i = 0; i < 2; ++i) {
+  double color[2][3] = {
+    {5.0, 0.0, 0.0},
+    {0.0, 5.0, 0.0}
+  };
+  for(auto itr = std::begin(axis); itr != std::end(axis); ++itr) {
     glBegin(GL_LINES);
-    glColor3dv(axis[2 * i + 1]);
-    glVertex3dv(axis[2 * i]);
-    glVertex3dv(axis[2 * i + 1]);
+    glColor3dv(color[itr - std::begin(axis)]);
+    glVertex3dv(axis[itr - std::begin(axis)][0]);
+    glVertex3dv(axis[itr - std::begin(axis)][1]);
     glEnd();
-  }
-}
-
-void pickMouse(int x, int y) {
-  unsigned int selectBuf[Buf] = {0};
-  int hits, viewport[4];
-  glGetIntegerv(GL_VIEWPORT, viewport);
-  glSelectBuffer(Buf, selectBuf);
-  glRenderMode(GL_SELECT);
-  glInitNames();
-  glPushName(0);
-  glMatrixMode(GL_PROJECTION);
-  glPushMatrix();
-    glLoadIdentity();
-    gluPickMatrix(x, height - y, 5.0, 5.0, viewport);
-    gluPerspective(60.0, (double)viewport[2] / viewport[3] , 1.0, 20.0);
-    glMatrixMode(GL_MODELVIEW);
-    drawHandle(GL_SELECT); // render with selection mode
-    glMatrixMode(GL_PROJECTION);
-  glPopMatrix();
-  glMatrixMode(GL_MODELVIEW);
-  hits = glRenderMode(GL_RENDER); // hit number
-  if(!hits) { return; }
-  double near = 1.0f,
-         far = 1.0f;
-  GLuint* ptr;
-  ptr = (GLuint*) selectBuf;
-  object_depth = INT_MAX;
-  for(int i=0; i < hits; ++i) {
-    unsigned int objs = *(ptr++);
-    near = (float) *(ptr++) / 0x7fffffff;
-    far = (float) *(ptr++) / 0x7fffffff;
-    object_depth = min(near, object_depth);
-    for(int j=0; j < objs; ++j, ++ptr) {
-      if(*ptr) { object_number = *ptr; }
-    }
   }
 }
 
@@ -206,36 +105,17 @@ void display(void) {
   glEnable(GL_DEPTH_TEST);
   glLoadIdentity();
   gluLookAt(0.0, 0.0, 5.0, 0, 0, 0, 0.0, 1.0, 0.0);
-  stressCircle.draw();
-  drawHandle(GL_RENDER);
   drawAxis();
+  stressCircle.updateVal();
+  stressCircle.drawCircle();
+  stressCircle.drawHandles(GL_RENDER);
   glFlush();
   glutSwapBuffers();
 }
 
-void dragObject(int x, int y) {
-  if(!move_flag || !object_depth) { return; }
-  double xx[3], mvMatrix[16], pjMatrix[16];
-  int viewport[4];
-  glGetIntegerv(GL_VIEWPORT, viewport);
-  glGetDoublev(GL_MODELVIEW_MATRIX, mvMatrix);
-  glGetDoublev(GL_PROJECTION_MATRIX, pjMatrix);
-  gluUnProject(x, height - y, object_depth/2, mvMatrix, pjMatrix, viewport, &xx[0], &xx[1], &xx[2]);
-  if(object_number == 1) {
-    stressCircle.update(xx[0], stressCircle.sigma_y, xx[1]);
-    sigma_x_box->set_float_val(stressCircle.sigma_x);
-    tau_box->set_float_val(stressCircle.tau);
-  }
-  else if(object_number == 2) {
-    stressCircle.update(stressCircle.sigma_x, xx[0], -xx[1]);
-    sigma_y_box->set_float_val(stressCircle.sigma_y);
-    tau_box->set_float_val(stressCircle.tau);
-  }
-}
-
 void reshape (int w, int h) {
-  width = w;
-  height = h;
+  stressCircle.windowWidth = w;
+  stressCircle.windowHeight = h;
   GLUI_Master.auto_set_viewport();
   int viewport[4];
   glGetIntegerv(GL_VIEWPORT, viewport);
@@ -250,17 +130,25 @@ void keyboard (unsigned char key, int x, int y) {
 }
 
 void mouse(int button, int state, int x, int y) {
-  mouse_button = button;
-  if(state == GLUT_DOWN && move_flag == 0) {
-    move_flag = !move_flag;
-    pickMouse(x, y);
+  switch(button) {
+    case GLUT_LEFT_BUTTON :
+      if(state == GLUT_DOWN) { stressCircle.pickHandle(x, y); }
+      is_drag = !is_drag; 
+      break;
+    default: break;
   }
-  else if(state == GLUT_UP) {
-    object_number = 0;
-    object_depth = -1;
-    move_flag = 0;
-    mouse_button = -1;
-  }
+}
+
+void motion(int x, int y) {
+  if(!is_drag) { return; }
+  stressCircle.dragHandle(x, y);
+  sigma_x_box->set_float_val(stressCircle.sigma_x);
+  sigma_y_box->set_float_val(stressCircle.sigma_y);
+  tau_box->set_float_val(stressCircle.tau);
+  angle_box->set_float_val(stressCircle.angle);
+  sigma_1_box->set_float_val(stressCircle.sigma_1);
+  sigma_2_box->set_float_val(stressCircle.sigma_2);
+  tau_max_box->set_float_val(stressCircle.tau_max);
 }
 
 void idle() {
@@ -268,6 +156,13 @@ void idle() {
   glutPostRedisplay();
 }
 
-void gluiExit(int num) {
-	exit(0);
+void gruiReset(int num) {
+  stressCircle.resetVal();
+  sigma_x_box->set_float_val(stressCircle.sigma_x);
+  sigma_y_box->set_float_val(stressCircle.sigma_y);
+  tau_box->set_float_val(stressCircle.tau);
+  angle_box->set_float_val(stressCircle.angle);
+  sigma_1_box->set_float_val(stressCircle.sigma_1);
+  sigma_2_box->set_float_val(stressCircle.sigma_2);
+  tau_max_box->set_float_val(stressCircle.tau_max);
 }
--- stress_circle.hpp
+++ stress_circle.hpp
@@ -1,38 +1,215 @@
 #ifndef   STRESS_CIRCLE_HPP
 #define   STRESS_CIRCLE_HPP
 
+#include <climits>
+#include <cmath>
+
+const float SIGMA_X_INITIAL = 2.0;
+const float SIGMA_Y_INITIAL =-2.0;
+const float TAU_INITIAL = 0.0;
+
 struct StressCircle {
-  StressCircle();
-  float sigma_x, sigma_y, tau;
-  static const int CircleDivisionNum = 100;
+  StressCircle(unsigned int name1, unsigned int name2, unsigned int w, unsigned int h);
+  float sigma_x, sigma_y, tau, sigma_1, sigma_2, tau_max, angle;
+  unsigned int x_handle_name;
+  unsigned int y_handle_name;
+  unsigned int angle_handle_name = 3;
+  unsigned int windowWidth = 800;
+  unsigned int windowHeight = 600;
+  unsigned int object_name = 0;
+  double object_depth = -1;
+  const int CIRCLE_DIVISION_NUM = 100;
+  const int HANDLE_DIVISION_NUM = 4;
+  const float HANDLE_RADIUS = 0.1;
+  const float SMALL_HANDLE_RADIUS = 0.05;
+  const int BUF = 512;
+  /**
+  * Return x coordination of the center of stress circle.
+  */
   float center();
+  /**
+  * Return radius of stress circle.
+  */
   float radius();
-  void update(float sigma_x, float sigma_y, float tau);
-  void draw();
+  /**
+  * Update stress values.
+  */
+  void updateStress(float sigma_x, float sigma_y, float tau);
+  /**
+  * Update dependent variables.
+  */
+  void updateVal();
+  /**
+  * Update all values.
+  */
+  void resetVal();
+  /**
+  * Render stress circle.
+  */
+  void drawCircle();
+  /**
+  * Render handles.
+  * @param mode current GL matrix mode
+  */
+  void drawHandles(GLenum mode);
+  /**
+  * Process mouse picking.
+  * @param x mouse x coordination
+  * @param y mouse y coordination
+  */
+  void pickHandle(int x, int y);
+  /**
+  * Update dragged handle's position.
+  * @param x mouse x coordination
+  * @param y mouse y coordination
+  */
+  void dragHandle(int x, int y);
 };
 
-StressCircle::StressCircle() : sigma_x(2.0),sigma_y(-2.0),tau(0.0) {};
+StressCircle::StressCircle(unsigned int name1, unsigned int name2, unsigned int w, unsigned int h) :
+  sigma_x(SIGMA_X_INITIAL),sigma_y(SIGMA_Y_INITIAL),tau(TAU_INITIAL),
+  x_handle_name(name1), y_handle_name(name2), windowWidth(w), windowHeight(h) {
+  updateVal();
+};
 
 float StressCircle::center() { return (sigma_x + sigma_y) / 2; }
 
 float StressCircle::radius() { return std::sqrt(std::pow(sigma_x - sigma_y, 2) / 4 + std::pow(tau, 2)); }
 
-void StressCircle::update(float sigma_x_new, float sigma_y_new, float tau_new){
+void StressCircle::updateStress(float sigma_x_new, float sigma_y_new, float tau_new) {
   sigma_x = sigma_x_new;
   sigma_y = sigma_y_new;
   tau = tau_new;
+  updateVal();
 }
 
-void StressCircle::draw() {
+void StressCircle::updateVal() {
+  angle = std::atan2(2 * tau, sigma_x - sigma_y) / 2.0 / M_PI * 180;
+  sigma_1 = center() + radius();
+  sigma_2 = center() - radius();
+  tau_max = radius();
+}
+
+void StressCircle::resetVal() {
+    updateStress(SIGMA_X_INITIAL, SIGMA_Y_INITIAL, TAU_INITIAL);
+}
+
+void StressCircle::drawCircle() {
   glColor3f(1.0, 1.0, 1.0);
+  glLineWidth(2);
   glBegin(GL_LINES);
-  for (int i = 0; i <= CircleDivisionNum; ++i) {
-    double theta = 2.0 * M_PI * i / CircleDivisionNum;
-    glVertex2d(radius() * cos(theta) + center(), radius() * sin(theta));
-    theta = 2.0 * M_PI * (i + 1) / CircleDivisionNum;
-    glVertex2d(radius() * cos(theta) + center(), radius() * sin(theta));
+  for (int i = 0; i < CIRCLE_DIVISION_NUM; ++i) {
+    double theta = 2.0 * M_PI * i / CIRCLE_DIVISION_NUM;
+    glVertex3d(radius() * std::cos(theta) + center(), radius() * std::sin(theta), -0.01);
+    theta = 2.0 * M_PI * (i + 1) / CIRCLE_DIVISION_NUM;
+    glVertex3d(radius() * std::cos(theta) + center(), radius() * std::sin(theta), -0.01);
   }
   glEnd();
+  glColor3f(1.0, 0.0, 0.0);
+  glBegin(GL_POLYGON);
+  for (int i = 0; i < CIRCLE_DIVISION_NUM; ++i) {
+    double theta = 2.0 * M_PI * i / CIRCLE_DIVISION_NUM;
+    glVertex2d(SMALL_HANDLE_RADIUS * std::cos(theta) + sigma_1, SMALL_HANDLE_RADIUS * std::sin(theta));
+  }
+  glEnd();
+  glColor3f(0.0, 0.0, 1.0);
+  glBegin(GL_POLYGON);
+  for (int i = 0; i < CIRCLE_DIVISION_NUM; ++i) {
+    double theta = 2.0 * M_PI * i / CIRCLE_DIVISION_NUM;
+    glVertex2d(SMALL_HANDLE_RADIUS * std::cos(theta) + sigma_2, SMALL_HANDLE_RADIUS * std::sin(theta));
+  }
+  glEnd();
+}
+
+void StressCircle::drawHandles(GLenum mode) {
+  if(mode == GL_SELECT) { glLoadName(x_handle_name); }
+  glColor3f(1.0, 0.0, 0.0);
+  glBegin(GL_QUADS);
+  for(int i = 0; i < HANDLE_DIVISION_NUM; ++i) {
+    double theta = 2.0 * M_PI * i / HANDLE_DIVISION_NUM + M_PI / 4;
+    glVertex2f(sigma_x + HANDLE_RADIUS * std::cos(theta), tau + HANDLE_RADIUS * std::sin(theta));
+  }
+  glEnd();
+  if(mode == GL_SELECT) { glLoadName(y_handle_name); }
+  glColor3f(0.0, 0.0, 1.0);
+  glBegin(GL_QUADS);
+  for(int i = 0; i < HANDLE_DIVISION_NUM; ++i) {
+    double theta = 2.0 * M_PI * i / HANDLE_DIVISION_NUM + M_PI / 4;
+    glVertex2f(sigma_y + HANDLE_RADIUS * std::cos(theta),-tau + HANDLE_RADIUS * std::sin(theta));
+  }
+  glEnd();
+  if(mode == GL_SELECT) { glLoadName(angle_handle_name); }
+  glColor3f(0.0, 1.0, 1.0);
+  glLineWidth(5);
+  glBegin(GL_LINES);
+  glVertex2d(sigma_x, tau);
+  glVertex2d(sigma_y,-tau);
+  glEnd();
+  glLineWidth(1);
+  glFlush();
+}
+
+void StressCircle::pickHandle(int x, int y) {
+  int viewport[4];
+  glGetIntegerv(GL_VIEWPORT, viewport);
+  unsigned int selectBuf[BUF];
+  glSelectBuffer(BUF, selectBuf);
+  glRenderMode(GL_SELECT);
+  glInitNames();
+  glPushName(0);
+  glMatrixMode(GL_PROJECTION);
+  glPushMatrix();
+  glLoadIdentity();
+  gluPickMatrix(x, windowHeight - y, 5.0, 5.0, viewport);
+  gluPerspective(60.0, (double)viewport[2] / viewport[3] , 1.0, 20.0);
+  glMatrixMode(GL_MODELVIEW);
+  drawHandles(GL_SELECT); // render with selection mode
+  glMatrixMode(GL_PROJECTION);
+  glPopMatrix();
+  glMatrixMode(GL_MODELVIEW);
+  int hits = glRenderMode(GL_RENDER); // hit number
+  object_name = -1;
+  object_depth = INT_MAX;
+  GLuint* ptr = (GLuint*)selectBuf;
+  for(int i = 0; i < hits; ++i) {
+    unsigned int objs = *(ptr++);
+    double near = ((double) *(ptr++) / INT_MAX / 2);
+    ptr++; // far
+    for(int j = 0; j < objs; ++j, ++ptr) {
+      if(object_depth > near) {
+        object_depth = near;
+        object_name = *ptr;
+      }
+    }
+  }
+}
+
+void StressCircle::dragHandle(int x, int y) {
+  double world_x, world_y,  world_z, mvMatrix[16], pjMatrix[16];
+  int viewport[4];
+  glGetIntegerv(GL_VIEWPORT, viewport);
+  glGetDoublev(GL_MODELVIEW_MATRIX, mvMatrix);
+  glGetDoublev(GL_PROJECTION_MATRIX, pjMatrix);
+  gluUnProject(x, windowHeight - y, object_depth, mvMatrix, pjMatrix, viewport, &world_x, &world_y, &world_z);
+  if(object_name == 1) {
+    updateStress(world_x, sigma_y, world_y);
+  }
+  else if(object_name == 2) {
+    updateStress(sigma_x, world_x,-world_y);
+  }
+  else if(object_name == 3) {
+    double theta = std::atan2(world_y, world_x - center());
+    if((tau && world_y) || (!tau && !world_y)) { // sigma_x side
+      updateStress(center() + radius() * std::cos(theta), 
+                   center() - radius() * std::cos(theta),
+                   radius() * std::sin(theta));
+    }
+    else { // sigma_y side
+      updateStress(center() - radius() * std::cos(theta), 
+                   center() + radius() * std::cos(theta),
+                   radius() * std::sin(theta));
+    }
+  }
 }
 
 #endif // STRESS_CIRCLE_HPP
